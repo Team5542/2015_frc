262409f7fb860ee2bb5e48625e29de8eff735c92
weeryan17 <rmohle17@bhmschools.org>
2015-01-21 15:44:37 -0600

    Added capibality for the arm (just need to do button stuff now)

--- Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/ArmManipulator.java
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/ArmManipulator.java
@@ -1,11 +1,30 @@
 package org.usfirst.frc.team5542.robot.subsystems;
 
+import org.usfirst.frc.team5542.robot.OI;
+import org.usfirst.frc.team5542.robot.Robot;
+import org.usfirst.frc.team5542.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class ArmManipulator extends Subsystem {
+	
+	private Talon aMotor;
+	
+    private ArmManipulator(){
+    	aMotor = new Talon(RobotMap.aMotor);
+    }
+
+    	public void arm(double input){
+        	if (input > 1.0 || input < -1.0)
+        		throw new IllegalArgumentException();
+        	aMotor.set(input);
+    }
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
@@ -14,5 +33,6 @@ public class ArmManipulator extends Subsystem {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
+
 }
 
