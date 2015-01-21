380b331f9addc3350dafbb89c5d1d14dc6e2d440
weeryan17 <rmohle17@bhmschools.org>
2015-01-21 14:53:22 -0600

    add subsystem ArmManipulator

---
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/ArmManipulator.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team5542.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class ArmManipulator extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
