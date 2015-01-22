3bc756d9ebb5cd025a89ac2dc1f712a06a736b91
weeryan17 <rmohle17@bhmschools.org>
2015-01-22 17:02:49 -0600

    Cleaned up stuff

--- Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/Arm.java
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/Arm.java
@@ -2,7 +2,6 @@ package org.usfirst.frc.team5542.robot.subsystems;
 
 
 import org.usfirst.frc.team5542.robot.RobotMap;
-import org.usfirst.frc.team5542.robot.commands.Arm;
 import org.usfirst.frc.team5542.robot.commands.UserArm;
 
 import edu.wpi.first.wpilibj.Talon;
