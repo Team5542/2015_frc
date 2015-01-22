0411d80a003a6eb87237c7d8eecda8773d686d6b
weeryan17 <rmohle17@bhmschools.org>
2015-01-22 16:54:47 -0600

    Acctuly got the arm working

--- Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/Arm.java
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/Arm.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team5542.robot.subsystems;
 
 
 import org.usfirst.frc.team5542.robot.RobotMap;
+import org.usfirst.frc.team5542.robot.commands.Arm;
 import org.usfirst.frc.team5542.robot.commands.UserArm;
 
 import edu.wpi.first.wpilibj.Talon;
@@ -23,7 +24,13 @@ public class Arm extends Subsystem {
         		throw new IllegalArgumentException();
         	aMotor.set(input);
     }
-    
+    	public static Arm instance;
+    	
+    	public static Arm getInstance(){
+    		if (instance == null)
+    			instance = new Arm();
+    		return instance;
+    	}
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
