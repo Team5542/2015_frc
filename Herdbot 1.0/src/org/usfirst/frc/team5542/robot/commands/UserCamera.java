18cb57807bce9c367ad21497cdabb8b7a38c39ac
Ryan Bowers <rbowers314@gmail.com>
2015-02-03 16:47:09 -0600

    removed direct camera code

--- Herdbot 1.0/src/org/usfirst/frc/team5542/robot/commands/UserCamera.java
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/commands/UserCamera.java
@@ -16,7 +16,6 @@ public class UserCamera extends CommandBase {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	camera.startVideoFeed();
     }
 
     // Called repeatedly when this Command is scheduled to run
