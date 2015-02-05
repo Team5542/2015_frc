18ef5de7dc76a3ad14e2ac7dec0fdd7b2d7056e0
weeryan17 <rmohle17@bhmschools.org>
2015-02-05 16:41:15 -0600

    PDP stuff start

--- Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/PDP.java
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/PDP.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  *
  */
-public class Smart_Dashboard extends Subsystem {
+public class PDP extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
