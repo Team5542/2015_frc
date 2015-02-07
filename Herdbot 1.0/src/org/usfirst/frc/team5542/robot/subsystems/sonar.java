b65d959426d6a04889cbc2329d85222b9f01b190
Ethan Roadfeldt <Ethan@roadfeldt.com>
2015-02-07 17:11:35 -0600

    added basic outline of what needs to be done

--- Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/sonar.java
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/sonar.java
@@ -1,12 +1,14 @@
 package org.usfirst.frc.team5542.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-//math: voltage divided by 1024 for every 5 mm
-public class sonar extends Subsystem {
+//math: voltage divided by 1024 for every 5 mm//
 
+public class sonar extends Subsystem {
+//get the channel and voltage from here
 	protected void initDefaultCommand() {
+		//calculate the distance and add the unit here
 		// TODO Auto-generated method stub
 		
-	}
-
+		}
 }
+//send the distance to the driver station
