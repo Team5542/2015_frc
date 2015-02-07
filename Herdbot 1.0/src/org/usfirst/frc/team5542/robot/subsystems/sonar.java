48f8aef93c3d0b12d915f6978ebf2a4a1ae0637f
Ethan Roadfeldt <Ethan@roadfeldt.com>
2015-02-07 16:53:46 -0600

    added sonar extendes subsystem

--- Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/sonar.java
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/sonar.java
@@ -1,5 +1,12 @@
 package org.usfirst.frc.team5542.robot.subsystems;
 
-public class sonar {
+import edu.wpi.first.wpilibj.command.Subsystem;
+//math: voltage divided by 1024 for every 5 mm
+public class sonar extends Subsystem {
+
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
