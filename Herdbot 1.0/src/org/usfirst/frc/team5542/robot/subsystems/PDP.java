6ed7722cc016252aeb0c51e06b18857f3b2786b6
weeryan17 <rmohle17@bhmschools.org>
2015-02-08 15:22:19 -0600

    PDP stuff for all ports

--- Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/PDP.java
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/PDP.java
@@ -13,9 +13,22 @@ public class PDP extends Subsystem {
 	PowerDistributionPanel PDP = new PowerDistributionPanel();
 	
 	protected void initDefaultCommand() {
+		SmartDashboard.putNumber("Current", PDP.getCurrent(0));
+		SmartDashboard.putNumber("Current", PDP.getCurrent(1));
+		SmartDashboard.putNumber("Current", PDP.getCurrent(2));
+		SmartDashboard.putNumber("Current", PDP.getCurrent(3));
+		SmartDashboard.putNumber("Current", PDP.getCurrent(4));
+		SmartDashboard.putNumber("Current", PDP.getCurrent(5));
+		SmartDashboard.putNumber("Current", PDP.getCurrent(6));
+		SmartDashboard.putNumber("Current", PDP.getCurrent(7));
+		SmartDashboard.putNumber("Current", PDP.getCurrent(8));
+		SmartDashboard.putNumber("Current", PDP.getCurrent(9));
+		SmartDashboard.putNumber("Current", PDP.getCurrent(10));
+		SmartDashboard.putNumber("Current", PDP.getCurrent(11));
+		SmartDashboard.putNumber("Current", PDP.getCurrent(12));
+		SmartDashboard.putNumber("Current", PDP.getCurrent(13));
 		SmartDashboard.putNumber("Current", PDP.getCurrent(14));
-		
-		
+		SmartDashboard.putNumber("Current", PDP.getCurrent(15));
 	}
 
 }
