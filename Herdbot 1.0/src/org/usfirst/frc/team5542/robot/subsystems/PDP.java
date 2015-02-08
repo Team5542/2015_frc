2df8ff5f36e21060e547bde5f5605174eb13a356
weeryan17 <rmohle17@bhmschools.org>
2015-02-08 13:08:15 -0600

    i did something

--- Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/PDP.java
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/PDP.java
@@ -11,28 +11,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class PDP extends Subsystem {
 
-	public PDP(){
-		PowerDistributionPanel PDP;
 		PDP = new PowerDistributionPanel();
-	}
-	//currently use PDP channels 0 2 3 12 13 15
-	Sendable pdp0;
-	Sendable pdp2;
-	Sendable pdp3;
-	Sendable pdp12;
-	Sendable pdp13;
-	Sendable pdp14;
-	Sendable null1;
 	
-		pdp0 = PDP.getCurrent(0);
-		pdp2 = PDP.getCurrent(2);
-		pdp3 = PDP.getCurrent(3);
-		pdp12 = PDP.getCurrent(12);
-		pdp13 = PDP.getCurrent(13);
-		pdp15 = PDP.getCurrent(15);
-	  
+	SmartDashboard.putNumber("Current", PDP.getCurrent(7));
 	
-		
 	    // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
