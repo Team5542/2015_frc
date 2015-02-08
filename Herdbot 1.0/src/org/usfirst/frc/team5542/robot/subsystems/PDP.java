31ac76dc63f7c0e38896618fbfb374a9ad6054bf
weeryan17 <rmohle17@bhmschools.org>
2015-02-08 14:26:58 -0600

    did stuff (not working :( )

--- Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/PDP.java
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/PDP.java
@@ -1,27 +1,28 @@
 package org.usfirst.frc.team5542.robot.subsystems;
 
-import edu.wpi.first.wpilibj.NamedSendable;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
-import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- *
- */
 public class PDP extends Subsystem {
 
-		PDP = new PowerDistributionPanel();
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		PowerDistributionPanel PDP = new PowerDistributionPanel();
+	}
+
+
 	
-	SmartDashboard.putNumber("Current", PDP.getCurrent(7));
+	SmartDashboard.putNumber("Current", pdp.getCurrent(14));
 	
 	    // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
-    public void initDefaultCommand() {
+/*    public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
-    }
+    */
 }
 
 
