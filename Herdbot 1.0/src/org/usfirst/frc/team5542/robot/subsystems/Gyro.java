3df684685af8f22fe46b36fa4ced25a9aa2f8ba7
Ryan Bowers <rbowers314@gmail.com>
2015-02-09 17:17:34 -0600

    Added Gyro functionality

--- Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/Gyro.java
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/Gyro.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team5542.robot.subsystems;
 
+import org.usfirst.frc.team5542.robot.commands.GyroUpdate;
+
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -13,6 +15,15 @@ public class Gyro extends Subsystem {
 		gyro = new I2C(I2C.Port.kOnboard, address);
 		gyro.write(0b0100011, 0b00010000);
 	}
+	
+	private static Gyro instance;
+	
+	public static Gyro getInstance(){
+		if(instance == null)
+			instance = new Gyro();
+		return instance;
+	}
+	
 	
 	private static final double sensitivity = .01750;
 	
@@ -38,9 +49,30 @@ public class Gyro extends Subsystem {
 		return rates;
 	}
 
+	private double[] angles = new double[3];
+	private long lastTime;
+	
+	public void updateAngles(double[] rates){
+		long currentTime = System.currentTimeMillis();
+		int difference = (int)(currentTime - lastTime);
+		lastTime = currentTime;
+		for(int i = 0; i < 3; i++){
+			angles[i] = rates[i] * difference;
+		}
+	}
+	
+	public double getXangle(){
+		return angles[0];
+	}
+	public double getYangle(){
+		return angles[1];
+	}
+	public double getZangle(){
+		return angles[2];
+	}
+	
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+    	setDefaultCommand(new GyroUpdate());
     }
 }
 
