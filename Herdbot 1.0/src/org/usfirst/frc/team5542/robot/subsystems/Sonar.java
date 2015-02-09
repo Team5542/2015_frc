14e0736fcef7930d788b1676aadd767a6f8c7ebb
gunert <Ethan@roadfeldt.com>
2015-02-09 17:49:24 -0600

    Set up basic sonar code

--- Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/Sonar.java
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/Sonar.java
@@ -1,17 +1,23 @@
 package org.usfirst.frc.team5542.robot.subsystems;
 
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
-//math: voltage divided by 1024 for every 5 mm//
+//math: voltage divided by 2 for every 10 mm//
 /**
  * sonar sensing stuff
  */
 
+
 public class Sonar extends Subsystem {
+		AnalogInput sonar = new AnalogInput(0);
+		double volts = sonar.getVoltage();
+		double distance = volts/2;
 //get the channel and voltage from here
-	protected void initDefaultCommand() {
+	protected void initDefaultCommand() { 
+		System.out.println("cm from object:");
+		System.out.println(distance);
 		//calculate the distance and add the unit here
 		// TODO Auto-generated method stub
-		
 		}
 }
 //send the distance to the driver station
