73a91bd31c0d17e7b5db097a376a939b5f934b9c
weeryan17 <rmohle17@bhmschools.org>
2015-02-10 16:50:28 -0600

    changed stuff and add smart smartdashboard for sonar

--- Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/Sonar.java
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/Sonar.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team5542.robot.subsystems;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 //math: voltage divided by 2 for every 10 mm//
 /**
  * sonar sensing stuff
@@ -10,13 +11,16 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Sonar extends Subsystem {
 	AnalogInput sonar = new AnalogInput(0);//analog imput object
+	double distance;
 
 	public double getdistance(){
 		double volts = sonar.getVoltage(); //gets the voltage
-		double distance = volts/2; //divides the value given by volts by two
-		return distance; //returns distance in centimeters
+		distance = volts/2; //divides the value given by volts by two
+		return distance;
 	}
-	
+	public void distance() {
+		SmartDashboard.putNumber("Distance in cm", distance);
+	}
 	//get the channel and voltage from here
 	protected void initDefaultCommand() { 
 		// TODO Auto-generated method stub
