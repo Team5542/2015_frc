397de2dd54cc2e9b902b97b8d2e8213db8ad0774
weeryan17 <rmohle17@bhmschools.org>
2015-02-15 16:33:43 -0600

    this might work

---
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/commands/SDCS.java
@@ -0,0 +1,38 @@
+package org.usfirst.frc.team5542.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *Smartdashboard command sceam
+ */
+public class SDCS extends Command {
+
+    public SDCS() {
+    }
+    double CS() {
+    	return SmartDashboard.getNumber("Control Seam", 0);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
