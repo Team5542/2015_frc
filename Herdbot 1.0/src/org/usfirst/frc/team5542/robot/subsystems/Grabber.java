906cca9290ddeb3fb2a05facf4d70e2cf5b5632c
weeryan17 <rmohle17@bhmschools.org>
2015-02-15 13:33:53 -0600

    started work now go ahead sam

---
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/subsystems/Grabber.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team5542.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Grabber extends Subsystem {
+    
+	private Victor pullMotor;
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
