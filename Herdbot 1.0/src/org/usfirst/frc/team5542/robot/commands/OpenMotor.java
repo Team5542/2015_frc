7befa0e56740bb137f515e40ea40e9720e8c44a8
Ann <Ann@10.174.200.26>
2015-02-15 16:42:53 -0600

    OpenMotor commands

---
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/commands/OpenMotor.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team5542.robot.commands;
+
+import org.usfirst.frc.team5542.robot.OI;
+import org.usfirst.frc.team5542.robot.Robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class OpenMotor extends CommandBase {
+
+    public OpenMotor() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires (grabber);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	grabber.openMotor(.5);    	
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	grabber.openMotor(0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	grabber.openMotor(0);
+    }
+}
