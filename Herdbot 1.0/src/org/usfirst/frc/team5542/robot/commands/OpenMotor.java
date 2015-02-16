9dcd9b7cd7b1f3f3b6f2c4eb881c1426d8031b5b
Ann <Ann@10.131.200.15>
2015-02-16 13:59:22 -0600

    Changed OpenMotor command's function calls

--- Herdbot 1.0/src/org/usfirst/frc/team5542/robot/commands/OpenMotor.java
+++ Herdbot 1.0/src/org/usfirst/frc/team5542/robot/commands/OpenMotor.java
@@ -23,7 +23,7 @@ public class OpenMotor extends CommandBase {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	claw.openMotor(.5);    	
+    	claw.open();    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -33,12 +33,12 @@ public class OpenMotor extends CommandBase {
 
     // Called once after isFinished returns true
     protected void end() {
-    	claw.openMotor(0);
+    	claw.open();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	claw.openMotor(0);
+    	claw.open();
     }
 }
